287af4bb74f7d2b20179f24328b3e2cee9a9958e
Gjore Zaharchev <Gjore.Zaharchev@seavus.com>
2025-03-05 09:08:44 +0100

    Initial commit

---
+++ src/test/java/com/gjore/test/Test1.java
@@ -0,0 +1,23 @@
+package com.gjore.test;
+
+import io.qameta.allure.*;
+import io.qameta.allure.severity.SeverityLevel;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.severity.SeverityLevel.TRIVIAL;
+
+
+public class Test1 {
+
+    @Test
+    public void test1(){
+        System.out.println("This is my test");
+        Allure.description("This test attempts to log into the website using a login and a password. Fails if any error happens.\n\nNote that this test does not test 2-Factor Authentication.");
+        Allure.label("severity", "critical");
+        Allure.label("owner", "John Doe");
+        Allure.link("Website", "https://dev.example.com/");
+        Allure.issue("AUTH-123", "https://example.com/issues/AUTH-123");
+        Allure.tms("TMS-456", "https://example.com/tms/TMS-456");
+    }
+}
